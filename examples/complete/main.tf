cc1e96a7b13fa4fb37344b75ff7f5a67282dd227
Sai <sai.hein@caylent.com>
2023-01-05 21:52:43 -0500

    move files to complete folders

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,4 +1,16 @@
-module "this" {
-  source      = "../../"
-  secret_name = var.secret_name
+module "vpc_peering_cross_account" {
+  source = "../"
+
+  requester_aws_assume_role_arn             = var.requester_aws_assume_role_arn
+  requester_region                          = var.requester_region
+  requester_vpc_id                          = var.requester_vpc_id
+  requester_allow_remote_vpc_dns_resolution = var.requester_allow_remote_vpc_dns_resolution
+
+
+  accepter_aws_assume_role_arn             = var.accepter_aws_assume_role_arn
+  accepter_region                          = var.accepter_region
+  accepter_vpc_id                          = var.accepter_vpc_id
+  accepter_allow_remote_vpc_dns_resolution = var.accepter_allow_remote_vpc_dns_resolution
+
+  tags = var.tags
 }
